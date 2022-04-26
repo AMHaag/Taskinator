5966d7793c0c185eaf0118dd7dc8b29942cc1da8
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-04-25 20:22:04 -0400

    functionally complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  if (!taskNameInput || !taskTypeInput){
+      alert("Please fill out the form before submission.");
+      return false;
+  }
+  formEl.reset();
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
